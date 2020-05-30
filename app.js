cde0f7dac71cf178f5bc02bf370258113b704026
Heitor Bernardes Quaglia <heitor.bq@hotmail.com>
2020-05-30 01:44:16 -0300

    express running

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', function(req, res){
+    res.send('Hello from my library app');
+});
+
+app.listen(3000, function(){
+    console.log('LISTENIN ON 3000');
+});
