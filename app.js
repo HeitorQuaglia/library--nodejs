c3e45569e2297a204d3ab43c6c12347ee23c557e
Heitor Quaglia <heitor.bq@hotmail.com>
2020-05-31 02:24:51 -0300

    add debug features

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
 
 const app = express();
+
+app.use(morgan('combined'));
 
 app.get('/', function(req, res){
     res.send('Hello from my library app');
 });
 
 app.listen(3000, function(){
-    console.log('LISTENIN ON 3000');
+    console.log('LISTENIN ON PORT: ' + chalk.green(3000) );
 });
